f56c02ab7e79672e8ae00075eed69d47b958fef1
Kush <kush.bhatt96@yahoo.com>
2020-03-10 17:51:44 -0600

    Created Handler and Controller for retrieving Tickets

--- API/Controllers/TicketsController.cs
+++ API/Controllers/TicketsController.cs
@@ -2,10 +2,32 @@
  * REST endpoint for any requests regarding teams.
  */
 
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Application.Tickets;
+using Domain;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+
 namespace API.Controllers
 {
-    public class TicketsController: BaseController
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TicketsController : BaseController
     {
-        
+        //Any API controller needs a root, API attribute, and must derive from the Base Controller class as seen above
+        private readonly IMediator _mediator;
+        public TicketsController(IMediator mediator)    //Injecting MediatR
+        {
+            _mediator = mediator;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<List<Ticket>>> List ()       //We will be returning a list of tickets
+        {
+            return await _mediator.Send(new List.Query());          //Sending a message to the List Query Handler
+        }
+
+
     }
 }
---
+++ Application/Tickets/List.cs
@@ -0,0 +1,33 @@
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+using Domain;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+using Persistence;
+
+namespace Application.Tickets
+
+{
+    public class List
+    {
+
+        public class Query : IRequest<List<Ticket>>{}
+
+        public class Handler : IRequestHandler<Query, List<Ticket>>
+        {
+            private readonly DataContext _context;
+            public Handler(DataContext context)    //from now on controllers will only be responsible for http requests and response --> all business logic handlers be the Handler (MediatR)
+            {
+                _context = context;
+            }
+
+            public async Task<List<Ticket>> Handle(Query request, CancellationToken cancellationToken)    //
+            {
+                var tickets = await _context.Tickets.ToListAsync();
+                return tickets;
+            }
+        }
+
+    }
+}
