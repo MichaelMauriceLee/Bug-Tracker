760e4e48051528fd96b79231ba7b21fc0daca491
Kush <kush.bhatt96@yahoo.com>
2020-03-10 20:58:41 -0600

    Added Edit Ticket Functionality.

--- API/Controllers/TicketsController.cs
+++ API/Controllers/TicketsController.cs
@@ -40,5 +40,11 @@ namespace API.Controllers
             return await _mediator.Send(command);
         }
 
+        [HttpPut("{id}")]       //Edit a particular ticket --> pass in id as root parameter
+        public async Task<ActionResult<Unit>> Edit(Guid id, Edit.Command command){
+            command.Id = id;
+            return await _mediator.Send(command);
+        }
+
     }
 }
--- Application/Tickets/Edit.cs
+++ Application/Tickets/Edit.cs
@@ -1,10 +1,68 @@
 ﻿/*
  * Edit Object to edit a ticket
  */
+using System;
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+using Domain;
+using MediatR;
+using Persistence;
+
 namespace Application.Tickets
 {
     public class Edit
     {
-        //TODO fill in implementation
+        public class Command : IRequest        //no entities are returned, only an indicator for whether the operation was successful (i.e. a MediatR Unit)
+        {
+            public Guid Id { get; set; }      //User won't be able to edit, but we still need it here to determine the Ticket that the user wants to update
+            public string Title { get; set; }
+            public string Description { get; set; }
+            public string Category { get; set; }
+            public DateTime SubmissionDate { get; set; }
+            public DateTime AcceptanceDate { get; set; }
+            public DateTime? DueDate { get; set; }    //? specifies that this is optional --> gets rid of error in Handler because DateTime is not allowed to be null
+            public string Priority { get; set; }
+            public bool IsAccepted { get; set; }
+            public string SubmitterId { get; set; }
+            public virtual AppUser Submitter { get; set; }
+            public string TeamId { get; set; }
+            public virtual Team Team { get; set; }
+            public string AssigneeId { get; set; }
+            public virtual AppUser Assignee { get; set; }
+            public virtual ICollection<Comment> Comments { get; set; }
+        }
+
+        public class Handler : IRequestHandler<Command>
+        {
+            private readonly DataContext _context;
+            public Handler(DataContext context)
+            {
+                _context = context;
+            }
+            public async Task<Unit> Handle(Command request, CancellationToken cancellationToken)
+            {
+                
+                var ticket = await _context.Tickets.FindAsync(request.Id);
+
+                if(ticket == null){
+                    throw new Exception("Could not find activity");
+                }
+
+                //?? is the coalescing operator. If the left side of ?? is null, then the right hand side will get executed
+                ticket.Title = request.Title ?? ticket.Title;
+                ticket.Description = request.Description ?? ticket.Description;
+                ticket.Category = request.Category ?? ticket.Category;
+                ticket.DueDate = request.DueDate ?? ticket.DueDate;
+                ticket.Priority = request.Priority ?? ticket.Priority;
+
+                var success = await _context.SaveChangesAsync() > 0;     //if we get > 0, this means that our addition of the new ticket was successful
+
+                if (success) return Unit.Value;          //returns a 200 ok response if create operation was successful
+
+                throw new Exception("Problem saving changes");
+            }
+
+        }
     }
 }
