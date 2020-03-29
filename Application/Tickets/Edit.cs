f17a2937858aa1c947493aa3416ad106540be72b
Kush <kush.bhatt96@yahoo.com>
2020-03-28 18:40:06 -0600

    Added AssigneeId to Edit.cs

--- Application/Tickets/Edit.cs
+++ Application/Tickets/Edit.cs
@@ -71,6 +71,7 @@ namespace Application.Tickets
                 ticket.Priority = request.Priority ?? ticket.Priority;
                 ticket.SubmissionDate = request.SubmissionDate ?? ticket.SubmissionDate;
                 ticket.SubmitterId = request.SubmitterId ?? ticket.SubmitterId;
+                ticket.AssigneeId = request.AssigneeId ?? ticket.AssigneeId;
 
                 var success = await _context.SaveChangesAsync() > 0;     //if we get > 0, this means that our addition of the new ticket was successful
 
