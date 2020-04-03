1441c3f60e872bd55899ee549b15d0bfa783efb1
Michael Lee <m.maurice.lee@gmail.com>
2020-04-02 18:49:38 -0600

    Added backend statistics functionality with complex query to make Kush happy :D

---
+++ API/Controllers/StatisticsController.cs
@@ -0,0 +1,18 @@
+﻿using System.Threading.Tasks;
+using Application.Statistics;
+using MediatR;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers
+{
+    public class StatisticsController : BaseController
+    {
+        [HttpGet]
+        [Authorize]
+        public async Task<ActionResult<Statistics.StatisticsEnvelope>> Statistics()
+        {
+            return await Mediator.Send(new Statistics.Query());
+        }
+    }
+}
---
+++ Application/Statistics/Statistics.cs
@@ -0,0 +1,48 @@
+﻿using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using Application.Interfaces;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+using Persistence;
+
+namespace Application.Statistics
+{
+    public class Statistics
+    {
+        public class StatisticsEnvelope
+        {
+            public int numManagersWithBios { get; set; }
+        }
+
+        public class Query : IRequest<StatisticsEnvelope>
+        {
+        }
+
+        public class Handler : IRequestHandler<Query, StatisticsEnvelope>
+        {
+            private readonly DataContext _context;
+            private readonly IUserAccessor _userAccessor;
+
+            public Handler(DataContext context, IUserAccessor userAccessor)
+            {
+                _userAccessor = userAccessor;
+                _context = context;
+            }
+
+            public async Task<StatisticsEnvelope> Handle(Query request, CancellationToken cancellationToken)
+            {
+                var numManagersWithBios = 
+                    (from user in _context.Users
+                        join teamMember in _context.TeamMembers on user.Id equals teamMember.AppUserId
+                        where teamMember.IsManager && !user.Bio.Contains(null)
+                        select user).ToList().Distinct().Count();
+
+                return new StatisticsEnvelope
+                {
+                    numManagersWithBios = numManagersWithBios
+                };
+            }
+        }
+    }
+}
